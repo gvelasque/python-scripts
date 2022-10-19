d15a0064140ca57dd156c336608857b949e0bfe4
gvelasque <gerardo.velasquez1993@gmail.com>
2022-10-19 17:20:14 +0200

    367. Valid Perfect Square

---
+++ leetcode_ploblems/algorithms/math/p367_valid_perfect_square.py
@@ -0,0 +1,22 @@
+# 367. Valid Perfect Square
+
+
+def is_perfect_square(num: int) -> bool:
+    """
+    Given a positive integer num, write a function which returns True if
+    num is a perfect square else False.
+
+    Follow up: Do not use any built-in library function such as sqrt.
+
+    Example 1:
+        - Input: num = 16
+        - Output: true
+
+    Example 2:
+        - Input: num = 14
+        - Output: false
+
+    Constraints:
+        - 1 <= num <= 2^31 - 1
+    """
+    return ((num ** (1 / 2)) % 1) == 0
